189de366ecb717ebf6e5fefea545b99cd74fbef0
Ahammad Ali Pinjari <ahammad0814@gmail.com>
2025-02-14 10:44:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	for (let i = 0; i < str.length; i++){
+		int cnt = 0;
+		for (let j = i+1; j < str.length; j++){
+			if (str[i] == str[j]){
+				cnt++;
+			}
+		}
+		if (cnt == 1)return;
+	}
+	return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
